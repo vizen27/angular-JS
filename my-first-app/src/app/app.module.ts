7deacddd40fe4170d64e546660d82870a1700660
Duque, Kean <vizen_27@yahoo.com>
2021-04-30 09:42:10 +0800

    angular first-app
    init

---
+++ my-first-app/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { ServerComponent } from './server/server.component';
+import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
